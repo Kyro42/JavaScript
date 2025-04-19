16ce0ec8d07e460d4b3ce63af9e2f06522f4d898
Kairo Viana <116967676+Kyro42@users.noreply.github.com>
2025-04-19 19:17:33 -0300

    Aula11 done

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`Your car's velocity is: ${vel}Km/h`)
+if (vel > 60){
+    console.log('You exceeded the speed limit. FINED!')
+}
+console.log('Always wear a seat belt when driving!')
---
+++ aula11/ex09.js
@@ -0,0 +1,7 @@
+var country = 'USA'
+console.log(`Living in ${country}.`)
+if (country == 'Brazil'){
+    console.log('You are: Brazillian')
+} else {
+    console.log('You are: Foreigner')
+}
