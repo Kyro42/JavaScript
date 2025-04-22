056409afd762e7479d2d8b6abcca987fa137cfc9
Kairo Viana <116967676+Kyro42@users.noreply.github.com>
2025-04-22 18:45:52 -0300

    aula13 & 14 done

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+var cont = 0
+
+do{
+    cont ++
+    console.log(cont)
+} while (cont < 6)
+
+
+/*
+while (cont < 6){
+    cont ++
+    console.log(cont)
+}
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
